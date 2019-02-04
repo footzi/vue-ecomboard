0253ded2b2ef5171981a7eea2c2cafb546578159
Vladislav Manakov <footzi@mail.ru>
2019-02-04 21:06:41 +0300

    Настраивает деплой

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,7 @@ app.get('/api/test',(req: Request, res: Response) => {
     res.send({"menu": "1"});
 })
 
-app.use(serveStatic(path.join(__dirname, '../dist')));
+app.use(serveStatic(path.join(__dirname, '../')));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
