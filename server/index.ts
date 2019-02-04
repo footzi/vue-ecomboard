e5a6ce84f1e4240441da81acaa4d667230ea3de2
Vladislav Manakov <footzi@mail.ru>
2019-02-04 20:40:25 +0300

    Настраивает деплой

--- server/index.ts
+++ server/index.ts
@@ -2,12 +2,17 @@ import { Request, Response } from 'express';
 
 const express = require('express');
 const app = express();
+const serveStatic = require('serve-static');
+const path = require('path');
+const port = process.env.PORT || 3030;
 
 app.get('/api/test',(req: Request, res: Response) => {
     res.status(200);
     res.send({"menu": "1"});
 })
 
-app.listen(3030, () => {
-    console.log('Examplee app listening on port 3030!');
+app.use(serveStatic(path.join(__dirname, '../dist')));
+
+app.listen(port, () => {
+    console.log(`Examplee app listening on port ${port}`);
 });
